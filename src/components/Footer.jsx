c61d2c735580651db95d17b2781b2e9b19fdfe48
Carla Rocha Bovi <carlarochabovi08@gmail.com>
2023-06-04 12:14:07 -0300

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import {FaDribbbleSquare, FaFacebookSquare, FaGithubSquare, FaInstagram, FaTwitt
 
 function Footer() {
   return ( 
-    <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8git text-gray-300'>
+    <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
       <div>
         <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
         <p className='py-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique minus rem tempore quos amet praesentium? Enim tempore doloremque exercitationem esse voluptatem, quasi eius necessitatibus rem corporis eveniet eos perferendis nostrum!</p>
